b7a5b6b7dfdc8e82adaf2b24ca268689009a6c4c
LuaxY <yann.guineau@voidmx.net>
2017-03-07 14:56:59 +0100

    Add Sentry exceptions report

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xs-6 col-sm-4 col-md-4" v-if="students[0]" v-cloak>
-    <!--<img :src="'<?php echo URL::asset('imgs/students') ?>/' + students[<?php echo $id ?>].uid + '.jpg'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>-->
-    <img :src="'http://graph.facebook.com/'+ students[<?php echo $id ?>].fbId +'/picture/picture?type=large'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>
+    <img :src="'<?php echo URL::asset('imgs/students') ?>/' + students[<?php echo $id ?>].uid + '.jpg'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>
+    <!--<img :src="'https://graph.facebook.com/'+ students[<?php echo $id ?>].fbId +'/picture/picture?type=large'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>-->
     <h3>{{ students[<?php echo $id ?>].name }}</h3>
     <h4>{{ students[<?php echo $id ?>].promotion }}</h4>
     <h4>
