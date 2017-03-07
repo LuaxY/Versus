d4f6e26bc4025200a29aa43fd695c8e026449e3b
LuaxY <yann.guineau@voidmx.net>
2017-03-07 18:34:00 +0100

    WIP move to Semantic UI

--- resources/views/student.blade.php
+++ resources/views/student.blade.php
@@ -1,11 +1,18 @@
-<div class="col-xs-6 col-sm-4 col-md-4" v-if="students[0]" v-cloak>
-    <img :src="'<?php echo URL::asset('imgs/students') ?>/' + students[<?php echo $id ?>].uid + '.jpg'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>
-    <!--<img :src="'https://graph.facebook.com/'+ students[<?php echo $id ?>].fbId +'/picture/picture?type=large'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>-->
-    <h3>{{ students[<?php echo $id ?>].name }}</h3>
-    <h4>{{ students[<?php echo $id ?>].promotion }}</h4>
-    <h4>
-        <i :class="'fa fa-' + (students[<?php echo $id ?>].sex == 'M' ? 'mars' : 'venus')" aria-hidden="true"></i>
-        <a :href="'https://facebook.com/' + students[<?php echo $id ?>].fbId" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
-    </h4>
-    <button @click="vote({{ $id }})" type="button" class="btn btn-primary choose">VOTER</button>
+<div class="ui card" v-if="students[0]" v-cloak>
+    <div class="image">
+        <img :src="'<?php echo URL::asset('imgs/students') ?>/' + students[<?php echo $id ?>].uid + '.jpg'" :alt="students[<?php echo $id ?>].name" class="img-thumbnail"/>
+    </div>
+    <div class="content">
+        <a class="header">{{ students[<?php echo $id ?>].name }}</a>
+        <div class="description">{{ students[<?php echo $id ?>].promotion }}</div>
+    </div>
+    <div class="content">
+        <span class="right floated">
+            <a :href="'https://facebook.com/' + students[<?php echo $id ?>].fbId" target="_blank"><i class="facebook square icon"></i></a>
+        </span>
+        <i :class="(students[<?php echo $id ?>].sex == 'M' ? 'man' : 'woman') + ' icon'"></i> {{ students[<?php echo $id ?>].sex == 'M' ? 'Homme' : 'Femme' }}
+    </div>
+    <div class="extra content">
+        <button @click="vote({{ $id }})" type="button" class="ui button primary fluid">VOTER</button>
+    </div>
 </div>
--- resources/views/versus.blade.php
+++ resources/views/versus.blade.php
@@ -14,8 +14,9 @@
         <!-- VueJS -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.2.1/vue.min.js"></script>
 
-        <!-- Bootstrap -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- Semantic UI -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.css" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.9/semantic.min.js"></script>
 
         <!-- Font Awesome -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -24,7 +25,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/fingerprintjs2/1.5.0/fingerprint2.min.js"></script>
 
         <!-- App -->
-        <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
+        <!--<link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">-->
         <script src="{{ URL::asset('js/app.js') }}"></script>
     </head>
     <body>
@@ -32,34 +33,63 @@
             <div class="header clearfix">
                 <h3 class="text-muted"><img src="{{ URL::asset('imgs/header.png') }}" alt="Logo" class="" width="50"/> Versus</h3>
             </div>
-            <div class="jumbotron vote">
-                <div class="row">
-                    @include('student', ['id' => 0])
-                    <div class="col-xs-6 col-xs-pull-3 col-sm-4 col-md-4 absolute">
-                        <img src="{{ URL::asset('imgs/vs.png') }}" alt="Versus" class="vs"/>
+
+            <div class="ui segment attached">
+                <div class="ui three column grid">
+                    <div class="row">
+                        <div class="column">
+                            @include('student', ['id' => 0])
+                        </div>
+                        <div class="column">
+                            <img src="{{ URL::asset('imgs/vs.png') }}" alt="Versus" class="vs"/>
+                        </div>
+                        <div class="column">
+                            @include('student', ['id' => 1])
+                        </div>
                     </div>
-                    @include('student', ['id' => 1])
                 </div>
             </div>
-            <div class="jumbotron filters">
+
+            <div class="ui bottom attached warning message">
+                <i class="warning icon"></i> Version démo
+            </div>
+
+            <div class="ui segment">
                 <h3>Sexes</h3>
-                <label><input type="checkbox" v-model="selectAllSexes"> Tout cocher</label>
-                <table class="table table-bordered table-striped">
+                <div class="ui checkbox">
+                    <input type="checkbox" v-model="selectAllSexes" id="selectAllSexes">
+                    <label for="selectAllSexes">Tout cocher</label>
+                </div>
+                <table class="ui celled table">
                     <tr v-for="sex in sexes">
-                        <td><input type="checkbox" v-model="selectedSex" :id="sex" :value="sex"></td>
-                        <td><label :for="sex">@{{ sex }}</label></td>
+                        <td>
+                            <div class="ui checkbox">
+                                <input type="checkbox" v-model="selectedSex" :id="sex" :value="sex">
+                                <label :for="sex"l>@{{ sex }}</label>
+                            </div>
+                        </td>
                     </tr>
                 </table>
                 <h3>Promotions</h3>
-                <label><input type="checkbox" v-model="selectAllPromotions"> Tout cocher</label>
-                <div class="row">
-                    <div class="col-xs-12 col-sm-4" v-for="promotionsGroup in promotions">
-                        <table class="table table-bordered table-striped">
-                            <tr v-for="promotion in promotionsGroup">
-                                <td><input type="checkbox" v-model="selectedPromotions" :id="promotion" :value="promotion"></td>
-                                <td><label :for="promotion">@{{ promotion }}</label></td>
-                            </tr>
-                        </table>
+                <div class="ui checkbox">
+                    <input type="checkbox" v-model="selectAllPromotions" id="selectAllPromotions">
+                    <label for="selectAllPromotions">Tout cocher</label>
+                </div>
+                <br><br>
+                <div class="ui four column grid">
+                    <div class="row">
+                        <div class="column" v-for="promotionsGroup in promotions">
+                            <table class="ui celled table">
+                                <tr v-for="promotion in promotionsGroup">
+                                    <td>
+                                        <div class="ui checkbox">
+                                            <input type="checkbox" v-model="selectedPromotions" :id="promotion" :value="promotion">
+                                            <label :for="promotion">@{{ promotion }}</label>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
